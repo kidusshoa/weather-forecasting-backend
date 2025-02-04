1d4b0c85a4055a28d01aece4f5c8ce610082889d
kidusshoa <captainkevin2008@gmail.com>
2025-02-04 18:37:48 +0300

    migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class WeatherPrediction(models.Model):
+    date = models.DateField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    precipitation = models.FloatField()
+    wind_speed = models.FloatField()
+
+    def __str__(self):
+        return f"Weather on {self.date}"
